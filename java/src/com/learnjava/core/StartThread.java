d1681d88acf75bb437423f9527ffbc0a58c22a24
Jaishish Ranjan <jaishishranjan@Jaishishs-MacBook-Pro.local>
2024-09-25 03:19:49 +0530

    updated StartThread class

--- java/src/com/learnjava/core/StartThread.java
+++ java/src/com/learnjava/core/StartThread.java
@@ -1,2 +1,21 @@
-package com.learnjava.core;public class StartThread {
+package com.learnjava.core;
+
+/**
+ * @author jaishishranjan
+ * This class will extend to Thread class to introduce and run a thread.
+ */
+public class StartThread extends Thread{
+    public void run() {
+        System.out.println("I am in StartThread class.");
+    }
+
+    public static void main (String [] args) throws InterruptedException {
+        StartThread startThread = new StartThread();
+        /**
+         * starting the thread of StartThread
+         * Invoking Thread using start() method
+         */
+        startThread.start();
+        //startThread.wait(10);
+    }
 }
